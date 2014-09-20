1d3c2d9e313d7aca9b0018a8681e66f6a4b5200e
Chenchal Subraveti <chenchals@gmail.com>
2014-09-19 21:02:27 -0500

    Added TODO comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function 'cacheSolve' computes inverse of the special 'matrix' 
+## created by function 'makeCacheMatrix' and caches the result for 
+## subsequent calls to get matrix inverse.
 
-## Write a short comment describing this function
-
+## Creates a special matrix with functions to get the matrix inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## 'x' shall be a square numeric or complex matrix. Logical are coerced to numetic
+  ## 'x' shall be invertible (no checks are made)
+  
+  ## makeCacheMatrix$set(x) Checks if a matrix is square, and resets its inverse to NULL
+  ## makeCacheMatrix$get() Returns the matrix 'x'  
+  ## makeCacheMatrix$setInverse(matInv) Sets a matrix that is the inverse of matrix 'x'
+  ## makeCacheMatrix$getInverse() Returns the inverse of matrix 'x'
+  
+  
+  
 
 }
 
-
-## Write a short comment describing this function
+## Returns the inverse matrix of matrix 'x'
+## If cached matrix inverse of 'x' is not NULL returns value from cache
+## Else inverse matrix of 'x' is computed, chached, and then returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Assumptions:
+  ## 'x' is a square matrix
+  ## 'x' is invertiable
+  ## '...' and additional arguments that are passed to the 'solve(a,b,...)' function
+  
+  
 }
